8153d11125209d9f7a307fc8b94935dc5e441ec6
Ryan <Ryan@Ryan-PC>
2013-01-22 00:48:12 -0800

    Initial Commit

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Ryan\workspace\Week03_Dantzler\bin\AndroidManifest.xml : \
+C:\Users\Ryan\workspace\Week03_Dantzler\AndroidManifest.xml \
---
+++ gen/com/dantzler/week03_dantzler/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dantzler.week03_dantzler;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
